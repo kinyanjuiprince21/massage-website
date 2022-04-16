ea832952e45eed19b34025ab541a1c82f4329d6b
man_git <kinyanjuiprince20@gmail.com>
2022-04-16 17:21:05 +0300

    final project

--- about_carousel.js
+++ about_carousel.js
@@ -1,4 +1,4 @@
-var radius = 240; // how big of the radius
+var radius = 80; // how big of the radius
 var autoRotate = true; // auto rotate or not
 var rotateSpeed = -60; // unit: seconds/360 degrees
 var imgWidth = 150; // width of images (unit: px)
